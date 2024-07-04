2bacfb716e412f81b0219d5533a5d914d1f069fc
Stephan Ye <ye.stephan@gmail.com>
2024-07-04 08:56:42 +0200

    latest updates for setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} text-sm text-zinz-900 bg-[#E5E8EC] min-h-screen`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
