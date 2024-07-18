b66732b31121ab59f236daa100e54ff8fb268ee9
Stephan Ye <ye.stephan@gmail.com>
2024-07-18 18:31:59 +0200

    blocks for dashboard

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,4 +1,8 @@
 import Branding from "@/components/branding";
+import ContentBlock from "@/components/content-block";
+import PetDetails from "@/components/pet-details";
+import PetList from "@/components/pet-list";
+import SearchForm from "@/components/search-form";
 import Stats from "@/components/stats";
 
 export default function Page() {
@@ -7,6 +11,15 @@ export default function Page() {
       <div className="flex items-center justify-between text-white py-8">
         <Branding />
         <Stats />
+      </div>
+      <div>
+        <SearchForm />
+        <ContentBlock>
+          <PetList />
+        </ContentBlock>
+        <ContentBlock>
+          <PetDetails />
+        </ContentBlock>
       </div>
     </main>
   );
---
+++ src/components/pet-details.tsx
@@ -0,0 +1,3 @@
+export default function PetDetails() {
+  return <section className="h-full w-full"></section>;
+}
