4e44601c698ff66d00e9890d5e74f03861a1c887
Stephan Ye <ye.stephan@gmail.com>
2024-07-29 08:27:57 +0200

    latest dialog

--- src/components/pet-button.tsx
+++ src/components/pet-button.tsx
@@ -1,6 +1,14 @@
 import { PlusIcon } from "@radix-ui/react-icons";
 import { Button } from "./ui/button";
 import { act } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import PetForm from "./pet-form";
 
 type PetButtonProps = {
   actionType: "add" | "edit" | "checkout";
@@ -15,9 +23,19 @@ export default function PetButton({
 }: PetButtonProps) {
   if (actionType === "add") {
     return (
-      <Button size="icon">
-        <PlusIcon height={24} width={24} />
-      </Button>
+      <Dialog>
+        <DialogTrigger>
+          <Button size="icon">
+            <PlusIcon height={24} width={24} />
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Add a new pet</DialogTitle>
+          </DialogHeader>
+          <PetForm></PetForm>
+        </DialogContent>
+      </Dialog>
     );
   }
 
