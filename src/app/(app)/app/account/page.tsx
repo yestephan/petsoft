c49c829fac5e61dc9f241ff883bb7097ca1198d7
Stephan Ye <ye.stephan@gmail.com>
2024-09-29 11:03:19 +0200

    page changes

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,12 +1,19 @@
+import { redirect } from "next/navigation";
+
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
+import { auth } from "@/lib/auth";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  if (!session?.user) {
+    redirect("/login");
+  }
   return (
     <main>
       <H1 className="my-8 text-white">Your Account</H1>
       <ContentBlock className="h-[500px]">
-        <p>Logged in...</p>
+        <p>{session?.user?.email}</p>
       </ContentBlock>
     </main>
   );
